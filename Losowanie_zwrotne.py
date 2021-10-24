495ba6a523572a4210729cd95a9790be9507670d
aga-moj-nick <agnieszka.aga.krawczyk@gmail.com>
2021-10-24 16:53:35 +0200

    Losowanie zwrotne i bezzwrotne

--- Losowanie_zwrotne.py
+++ Losowanie_zwrotne.py
@@ -2,24 +2,24 @@ import random
 from tabulate import tabulate
 
 
-
 szczęśliwe_numerki =[]
 i = 0
 n = int(input("Podaj liczebność populacji: "))
 
-while i < 1:
+while i < 3:
     
-    r = random.sample(range(1, n+1), 9)
+    r = random.sample(range(1, n+1), 3)
 
     if szczęśliwe_numerki.count(r) == 0:
         szczęśliwe_numerki.append(r)
         i += 1
 
+
 table = [
     ["Liczba studentów: ", n],
-    ["Szczęśliwe numerki w pierwszym tygodniu zajęć to: ", r[0:3]],
-    ["Szczęśliwe numerki w drugim tygodniu zajęć to: ", r[3:6]],
-    ["Szczęśliwe numerki w trzecim tygodniu zajęć to: ", r[6:9]]
+    ["Szczęśliwe numerki w pierwszym tygodniu zajęć to: ", szczęśliwe_numerki[0]],
+    ["Szczęśliwe numerki w drugim tygodniu zajęć to: ", szczęśliwe_numerki[1]],
+    ["Szczęśliwe numerki w trzecim tygodniu zajęć to: ", szczęśliwe_numerki[2]]
 ]
 
 print(tabulate(table, headers='firstrow', tablefmt='fancy_grid'))
